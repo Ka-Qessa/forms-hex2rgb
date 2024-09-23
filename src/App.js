beae8b468b362ed7a015a67f7d02903887e68419
Irina Shibaeva <irina-kuppieva@yandex.ru>
2024-09-23 22:03:34 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ColorForm from './components/ColorConverter';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='task1'>
+        <ColorForm />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
